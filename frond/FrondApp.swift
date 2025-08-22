9e156f15f7251aa10e7d6347646c79558598b80c
Straight8ths <straight8ths@gmail.com>
2025-08-22 10:00:01 -0700

    Create FrondApp.swift

---
+++ frond/FrondApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrondApp.swift
+//  Frond
+//
+//  Created by Sebastian Bradford on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FrondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
