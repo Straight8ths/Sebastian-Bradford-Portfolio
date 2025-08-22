14970605b0a2d1830b2a09b9d7abfddf7c87b397
Straight8ths <straight8ths@gmail.com>
2025-08-22 10:00:21 -0700

    Create learnTrees.swift

---
+++ frond/learnTrees.swift
@@ -0,0 +1,17 @@
+//
+//  learnTrees.swift
+//  Frond
+//
+//  Created by Sebastian Bradford on 7/19/25.
+//
+
+import SwiftUI
+
+struct learnTrees: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    learnTrees()
+}
